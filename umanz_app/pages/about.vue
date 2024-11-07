f1dcfa9c9595b119c24db6ec5c33d0479945e399
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-07 19:25:27 +0200

    [init][aff]-Init front

---
+++ umanz_app/pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+    pageTransition: {
+        name: 'rotate'
+    }
+});
+</script>
+
+<template>
+    <section>
+        <p>This page will be displayed at the /about route.</p>
+
+        <NuxtLink to="/">home</NuxtLink>
+    </section>
+</template>
---
+++ umanz_app/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Welcome to the homepage</h1>
+
+        <NuxtLink to="/about">about</NuxtLink>
+
+        <AppAlert>
+            This is an auto-imported component
+        </AppAlert>
+    </div>
+</template>
