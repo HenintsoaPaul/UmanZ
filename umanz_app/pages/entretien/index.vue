a989e2f2b1e579c1920830151b61f4009309d9d1
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-09 21:04:05 +0200

    [entretien]-Sample list + validation + api

--- umanz_app/pages/entretien/index.vue
+++ umanz_app/pages/entretien/index.vue
@@ -2,7 +2,6 @@
 import axios from 'axios';
 import type { Entretien } from '~/types';
 
-// Columns a afficher
 const headers = [
     {
         key: 'id_entretien',
@@ -67,16 +66,62 @@ async function loadAnnonces() {
     }
 }
 
-// Appel de la fonction pour charger les données au chargement du composant
 loadAnnonces();
+
+const expand = ref({
+    openedRows: [],
+    row: {}
+})
+
+const note = ref<number>(0);
+
+const validerFn = async (id_entretien: number) => {
+    if (!note.value) {
+        const apiUrl = useRuntimeConfig().public.apiUrl;
+        try {
+            const params = {
+                note: note.value,
+                id_entretien: id_entretien
+            }
+
+console.log(params);
+
+
+            const response = await axios.post(`${apiUrl}/entretien/candiat`, params);
+
+            if (response.status === 200) {
+                console.log('Valiation envoyée avec succès', params);
+            } else {
+                console.error('Erreur lors de la validation', response.data);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la requête API:', error);
+        }
+    }
+}
 </script>
 
 <template>
-    <h1 class="text-2xl font-bold mb-4">Liste des Entretiens</h1>
+    <div class="container mx-auto">
+        <h1 class="text-3xl font-bold mb-4 text-center">Liste des Entretiens</h1>
 
-    <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
-        <UInput v-model="q" placeholder="Filtrer les annonces..." />
+        <div class="flex justify-center mb-4">
+            <UInput v-model="q" placeholder="Filtrer les annonces..." class="w-full max-w-md px-4 py-2 rounded-lg" />
+        </div>
+
+        <UTable :columns="headers" :rows="filteredLignes" v-model:expand="expand">
+            <template #expand="{ row }">
+                <div class="p-4">
+                    <UInput v-model="note" placeholder="Ajouter la note..."
+                        class="w-full max-w-md px-4 py-2 rounded-lg" />
+
+
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        @click="validerFn(row.id_entretien)">
+                        Valider
+                    </button>
+                </div>
+            </template>
+        </UTable>
     </div>
-
-    <UTable :columns="headers" :rows="filteredLignes" />
 </template>
