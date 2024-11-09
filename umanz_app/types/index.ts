f497fe68c5b4483f5cf521a8d3b2d471efd0b086
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-09 07:11:55 +0200

    [cv]-Aff cv

--- umanz_app/types/index.ts
+++ umanz_app/types/index.ts
@@ -1,6 +1,6 @@
 export interface Absence {
     id_absence: number,
-    motif?: string ,
+    motif?: string,
     date_absence: Date,
     id_contrat: number,
 }
@@ -11,4 +11,33 @@ export interface Talent {
     prenom: string,
     mail: string,
     password: string,
+}
+
+export interface Competence {
+    id_competence: number;
+    competence: string;
+}
+
+export interface TalentCompetence {
+    id_competence: number,
+    id_talent: number,
+    point: number,
+}
+
+export interface Poste {
+    id_poste: number,
+    nom_poste: string,
+    description_poste?: string,
+}
+
+export interface ExperienceTalent {
+    id_poste: number,
+    id_talent: number,
+    ans: number,
+}
+
+export interface CV {
+    id_talent: number;
+    competences: TalentCompetence[],
+    experiences: ExperienceTalent[]
 }
