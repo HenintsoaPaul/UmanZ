317c370c9480229939cab73196ad9b1e5b9e6fda
Ryan <lizkaryan626@gmail.com>
2024-11-09 12:42:54 +0300

    liste conge

---
+++ rhAPI/src/main/java/mg/itu/rh/repository/CongeRepository.java
@@ -0,0 +1,15 @@
+package mg.itu.rh.repository;
+
+import mg.itu.rh.entity.Conge;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CongeRepository extends JpaRepository<Conge,Long> {
+    @Query("select new mg.itu.rh.entity.Conge(conge.idConge,conge.dateDebut,conge.nbJour,conge.motif) from Conge conge join conge.contrat contrat join contrat.talent talent where talent.idTalent=:idTalent")
+    public List<Conge> findCongeByIdTalent(@Param("idTalent") Long idTalent);
+}
