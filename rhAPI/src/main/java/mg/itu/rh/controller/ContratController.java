de1844174a01686a12099720c83f9f847830e188
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-14 07:58:28 +0200

    [back][contrat]-Add contrat

--- rhAPI/src/main/java/mg/itu/rh/controller/ContratController.java
+++ rhAPI/src/main/java/mg/itu/rh/controller/ContratController.java
@@ -1,16 +1,10 @@
 package mg.itu.rh.controller;
 
 import com.fasterxml.jackson.annotation.JsonView;
-import jakarta.persistence.Entity;
-import lombok.Data;
 import mg.itu.rh.dto.ContratDTO;
-import mg.itu.rh.dto.RuptureDTO;
 import mg.itu.rh.entity.Contrat;
-import mg.itu.rh.entity.Rupture;
 import mg.itu.rh.other.POV;
 import mg.itu.rh.service.ContratService;
-import mg.itu.rh.service.RuptureService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -55,6 +49,4 @@ public class ContratController {
     public Contrat save( @RequestBody ContratDTO contratDTO ) {
         return contratService.save( contratDTO );
     }
-
-
 }
