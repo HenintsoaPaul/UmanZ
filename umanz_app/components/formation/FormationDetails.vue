2ae41c0c116ca9bce427992685f8884c03a1c00e
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-14 08:55:57 +0200

    [aff][formation]-Liste formation disponible

---
+++ umanz_app/components/formation/FormationDetails.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { Formation } from '~/types';
+
+const props = defineProps<{
+    apiUrl: string;
+    idContrat: number;
+    formation: Formation;
+    participerFn: (idContrat: number, idFormation: number, apiUrl: string) => Promise<void>;
+}>();
+</script>
+
+<template>
+    <div class="mb-4">
+        <h3 class="text-xl font-semibold mb-2">Titre:</h3>
+        <p class="text-gray-700">{{ formation.nomFormation }}</p>
+    </div>
+
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="participerFn(props.idContrat, formation.idFormation, props.apiUrl)">
+        Candidater
+    </button>
+</template>
