8c54dec19ba9ec6f5652af205b00005d0f870452
Henintsoa Paul <henintsoapaul@gmail.com>
2024-11-17 08:40:59 +0200

    [fix]-Fix talent + compatibilite

--- umanz_app/pages/interne/formation/index.vue
+++ umanz_app/pages/interne/formation/index.vue
@@ -31,7 +31,7 @@ const idContrat = computed(() => Number(localStorage.getItem("idUser")));
                 class="w-full max-w-md px-4 py-2  rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
         </div>
 
-        <UTable :columns="headers" :rows="filteredLignes" v-model:expand="expand"
+        <UTable :columns="headers" :rows="filteredLignes ?? []" v-model:expand="expand"
             class="w-full shadow-md rounded-lg overflow-hidden">
             <template #expand="{ row }">
                 <div class="p-4">
