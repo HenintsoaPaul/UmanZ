30ea30c3378745c8bab8cf397264c9cdfd99175b
Tiarintsoa <rihantiana000@gmail.com>
2024-12-02 15:59:03 +0300

    To fix integration

--- umanz_app/components/paie/CorpsFichePaie.vue
+++ umanz_app/components/paie/CorpsFichePaie.vue
@@ -2,7 +2,7 @@
 
 import type {PaysLipsDetails} from "~/types";
 
-defineProps<{
+const props = defineProps<{
     paysLipsBrutDetails: PaysLipsDetails[];
 }>();
 
@@ -27,20 +27,19 @@ interface rowIrsa {
     surligne: boolean;
 }
 
-const dataIRSA: rowIrsa[] = [
-    { designation: 'Retenue CNaPS 1%', taux: '', montant: 20000.00, surligne: false },
-    { designation: 'Retenue sanitaire', taux: '', montant: 40834.09, surligne: false },
-    { designation: 'Tranche IRSA INF 350 0000', taux: '', montant: '', surligne: false },
-    { designation: 'Tranche IRSA I DE 350 0001 à 400 000', taux: '5%', montant: 2500.00, surligne: false },
-    { designation: 'Tranche IRSA I DE 400 0001 à 500 000', taux: '10%', montant: 10000.00, surligne: false },
-    { designation: 'Tranche IRSA I DE 500 001 à 600 000', taux: '15%', montant: 15000.00, surligne: false },
-    { designation: 'Tranche IRSA SUP 600 0000', taux: '20%', montant: 684515.00, surligne: false },
+const apiUrl = useRuntimeConfig().public.apiUrl;
 
-    { designation: 'TOTAL IRSA', taux: '', montant: 712015.00 , surligne: true},
-    { designation: 'Total des retenues', taux: '', montant: 772849.09 , surligne: true},
-    { designation: 'Autres indemnités', taux: '', montant: 0 , surligne: true},
-    { designation: 'Net à payer', taux: '', montant: 3310560.00 , surligne: true}
-];
+const { data: dataIRSA } = useFetch<PaysLipsDetails[]>(`${apiUrl}/talents/fiche-paie-retenue`, {
+    query: {
+        salaireBrute: props.paysLipsBrutDetails.find<PaysLipsDetails>((el) => {
+            return el.designation === "Salaire brute";
+        })?.montant
+    },
+});
+
+console.log(props.paysLipsBrutDetails.find<PaysLipsDetails>((el) => {
+    return el.designation === "Salaire brute";
+}, "Here");
 
 const highlightedClasses = (item: rowIrsa) => ({
   'bg-blue-500': item.surligne,
@@ -55,7 +54,7 @@ const highlightedClasses = (item: rowIrsa) => ({
 
         <br>
 
-        <UTable :columns="headersIRSA" :rows="dataIRSA">
+        <UTable :columns="headersIRSA" :rows="dataIRSA ?? []">
             <template #default="{ item }">
                 <tr :class="highlightedClasses(item)"></tr>
             </template>
