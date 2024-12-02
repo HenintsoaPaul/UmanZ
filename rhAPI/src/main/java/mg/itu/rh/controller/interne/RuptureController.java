4d3924886e189a8299393b41cd3912dd6591281c
Henintsoa Paul <henintsoapaul@gmail.com>
2024-12-02 23:17:30 +0300

    [metier]-fix RuptureController

--- rhAPI/src/main/java/mg/itu/rh/controller/interne/RuptureController.java
+++ rhAPI/src/main/java/mg/itu/rh/controller/interne/RuptureController.java
@@ -1,31 +1,97 @@
 package mg.itu.rh.controller.interne;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.AllArgsConstructor;
+import mg.itu.rh.constante.ConstanteEmail;
 import mg.itu.rh.dto.interne.RuptureDTO;
+import mg.itu.rh.dto.rupture.DemissionRequest;
+import mg.itu.rh.dto.rupture.RenvoiRequest;
+import mg.itu.rh.dto.rupture.RetraiteRequest;
 import mg.itu.rh.entity.interne.Rupture;
 import mg.itu.rh.other.POV;
+import mg.itu.rh.service.interne.EmailService;
 import mg.itu.rh.service.interne.RuptureService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@AllArgsConstructor
 @RestController
-@RequestMapping("/rupture")
+@RequestMapping( "/ruptures" )
 public class RuptureController {
-    @Autowired
-    private RuptureService ruptureService;
+    private final EmailService emailService;
+    private final RuptureService ruptureService;
 
-    /*
-    * {
-    *       "idContrat":5,
-    *       "idTypeRupture":4
-    * }
-    * */
+    @PostMapping( "/demission" )
+    public ResponseEntity<String> demission( @RequestBody DemissionRequest demissionRequest ) {
+        try {
+            ruptureService.saveDemission( demissionRequest );
+
+            String toEmail = ConstanteEmail.mailRh;
+            emailService.writeEmail( toEmail, demissionRequest.getSubject(), demissionRequest );
+            return ResponseEntity.ok( "Email envoyé avec succès." );
+        } catch ( Exception e ) {
+            return ResponseEntity.status( HttpStatus.INTERNAL_SERVER_ERROR )
+                    .body( "Erreur lors de l'envoi de l'email : " + e.getMessage() );
+        }
+    }
+
+    @PostMapping( "/retraite" )
+    public ResponseEntity<String> retraite( @RequestBody RetraiteRequest retraiteRequest ) {
+        try {
+            ruptureService.saveRetraite( retraiteRequest );
+
+            String toEmail = ConstanteEmail.mailRh;
+            emailService.writeEmail( toEmail, retraiteRequest.getSubject(), retraiteRequest );
+            return ResponseEntity.ok( "Email envoyé avec succès." );
+        } catch ( Exception e ) {
+            return ResponseEntity.status( HttpStatus.INTERNAL_SERVER_ERROR )
+                    .body( "Erreur lors de l'envoi de l'email : " + e.getMessage() );
+        }
+    }
+
+    @PostMapping( "/renvoi" )
+    public ResponseEntity<String> renvoi( @RequestBody RenvoiRequest renvoiRequest ) {
+        try {
+            ruptureService.saveRenvoi( renvoiRequest );
+
+            emailService.writeEmail( renvoiRequest.getEmail(), renvoiRequest.getSubject(), renvoiRequest );
+            return ResponseEntity.ok( "Email envoyé avec succès." );
+        } catch ( Exception e ) {
+            return ResponseEntity.status( HttpStatus.INTERNAL_SERVER_ERROR )
+                    .body( "Erreur lors de l'envoi de l'email : " + e.getMessage() );
+        }
+    }
+
+    @PostMapping( "/sendDossier" )
+    @JsonView( POV.Public.class )
+    public ResponseEntity<Map<String, String>> sendEmailDossier( @RequestBody Map<String, String> requestData ) {
+        Map<String, String> response = new HashMap<>();
+        String email = requestData.get( "email" );
+        String name = requestData.get( "name" );
+
+        if ( email == null || name == null ) {
+            response.put( "message", "Paramètre manquant" );
+            return ResponseEntity.badRequest().body( response );
+        }
+
+        try {
+            emailService.sendEmailWithAttachments( email, name );
+            response.put( "message", "Email envoyé avec succès à " + email );
+            return ResponseEntity.ok( response );
+        } catch ( Exception e ) {
+            response.put( "message", "Erreur lors de l'envoi de l'email: " + e.getMessage() );
+            return ResponseEntity.status( HttpStatus.INTERNAL_SERVER_ERROR ).body( response );
+        }
+    }
+
     @PostMapping
-    @JsonView(POV.Public.class)
-    public Rupture save(@RequestBody RuptureDTO ruptureDTO)throws Exception{
-        return ruptureService.save(ruptureDTO);
+    @JsonView( POV.Public.class )
+    public Rupture save( @RequestBody RuptureDTO ruptureDTO )
+            throws Exception {
+        return ruptureService.save( ruptureDTO );
     }
 }
