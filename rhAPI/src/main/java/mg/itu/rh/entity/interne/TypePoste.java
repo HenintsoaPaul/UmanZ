88857a4f0535f49ef12d3aaffb6f2d41fa5849ea
Ryan <lizkaryan626@gmail.com>
2024-12-03 13:58:22 +0300

    Improve serialization

--- rhAPI/src/main/java/mg/itu/rh/entity/interne/TypePoste.java
+++ rhAPI/src/main/java/mg/itu/rh/entity/interne/TypePoste.java
@@ -1,7 +1,9 @@
 package mg.itu.rh.entity.interne;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import lombok.Data;
+import mg.itu.rh.other.POV;
 
 @Entity
 @Data
@@ -10,9 +12,11 @@ public class TypePoste {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_type_poste")
+    @JsonView( { POV.Public.class, POV.Emp.class } )
     private Long idTypePoste;
 
     @Column(name = "nom_type_poste")
+    @JsonView( { POV.Public.class, POV.Emp.class } )
     String typePoste;
 
     @Column(name = "rang_type_poste")
