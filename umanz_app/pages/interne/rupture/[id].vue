64242b1c9c0fcc77a805dd076dc4f0f00650bad9
Tiarintsoa <rihantiana000@gmail.com>
2024-12-14 15:42:40 +0300

    Email sending form style

--- umanz_app/pages/interne/rupture/[id].vue
+++ umanz_app/pages/interne/rupture/[id].vue
@@ -7,69 +7,69 @@ const idContrat = computed(() => route.params.id);
 const router = useRouter();
 
 const formData = {
-  motif: "",
-  date: "",
+    motif: "",
+    date: "",
 }
 
 const apiUrl = useRuntimeConfig().public.apiUrl as string;
 
 async function handleSubmit() {
-  const payLoad = {
-    idContrat: idContrat.value,
-    motif: formData.motif,
-    date: formData.date
-  };
+    const payLoad = {
+        idContrat: idContrat.value,
+        motif: formData.motif,
+        date: formData.date
+    };
 
-  console.log(payLoad);
+    console.log(payLoad);
 
 
-  try {
-    const response = await axios.post(
-      `${apiUrl}/ruptures/renvoi`,
-      payLoad
-    );
-    alert("Email envoyé avec succès : " + response.data);
+    try {
+        const response = await axios.post(
+            `${apiUrl}/ruptures/renvoi`,
+            payLoad
+        );
+        alert("Email envoyé avec succès : " + response.data);
 
-    formData.motif = "";
-    formData.date = "";
+        formData.motif = "";
+        formData.date = "";
 
-    await router.push("/interne/rupture/rupturValidation");
-  } catch (error) {
-    console.error("Erreur lors de l'envoi de l'email:", error);
-    alert("Échec de l'envoi de l'email.");
-  }
+        await router.push("/interne/rupture/rupturValidation");
+    } catch (error) {
+        console.error("Erreur lors de l'envoi de l'email:", error);
+        alert("Échec de l'envoi de l'email.");
+    }
 }
 </script>
 
 <template>
-  <div class="content flex items-center justify-center font-mono">
-    <div class="border border-slate-50 p-8 rounded-lg shadow-md w-full max-w-md text-slate-500">
-      <h2 class="text-2xl font-bold mb-6 text-center">Licenciement</h2>
-      <UForm :state="formData" class="space-y-4" @submit="handleSubmit">
-        <UFormGroup label="Motif" name="motif">
-          <UInput id="motif" v-model="formData.motif" placeholder="Entrez le motif"
-            class="w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required />
-        </UFormGroup>
+    <div class="content flex items-center justify-center font-mono">
+        <div class="border border-slate-50 p-8 rounded-lg shadow-md w-full max-w-md text-slate-500">
+            <h2 class="text-2xl font-bold mb-6 text-center">Licenciement</h2>
+            <UForm :state="formData" class="space-y-4" @submit="handleSubmit">
+                <UFormGroup label="Motif" name="motif">
+                    <UInput id="motif" v-model="formData.motif" placeholder="Entrez le motif"
+                            class="w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required/>
+                </UFormGroup>
 
-        <UFormGroup label="Date" name="date">
-          <UInput id="date" v-model="formData.date" type="date"
-            class="w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required />
-        </UFormGroup>
+                <UFormGroup label="Date" name="date">
+                    <UInput id="date" v-model="formData.date" type="date"
+                            class="w-full focus:outline-none focus:ring-2 focus:ring-blue-500" required/>
+                </UFormGroup>
 
-        <div class="w-full flex justify-center">
-          <UButton type="submit"
-            class="w-1/2 bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center">
-            Licencier
-          </UButton>
+                <div class="w-full flex justify-center">
+                    <UButton type="submit"
+                             class="w-1/2 bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center">
+                        Licencier
+                    </UButton>
+                </div>
+            </UForm>
         </div>
-      </UForm>
     </div>
-  </div>
 </template>
 
 <style scoped>
 body {
-  font-family: "Inter", sans-serif;
+    font-family: "Inter", sans-serif;
 }
 
 .content {
--- umanz_app/pages/interne/rupture/rupturValidation.vue
+++ umanz_app/pages/interne/rupture/rupturValidation.vue
@@ -23,7 +23,7 @@ export default {
                 console.log('Données envoyées:', { email: this.email, name: this.name });
 
                 // Envoi de la requête POST
-                const response = await axios.post('http://localhost:911/talents/sendDossier', {
+                const response = await axios.post('http://localhost:911/ruptures/sendDossier', {
                     email: this.email,
                     name: this.name
                 }, {
@@ -63,52 +63,66 @@ export default {
 </script>
 
 <template>
-  <div class="container bg-gray-100 min-h-screen flex items-center justify-center">
-    <div class="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+  <div class="content flex items-center justify-center">
+    <div class="border border-slate-50 p-8 rounded-lg shadow-md w-full max-w-md text-slate-500">
       <h2 class="text-2xl font-semibold text-center mb-6">Envoyer un email avec pièces jointes</h2>
-      
+
       <form @submit.prevent="sendEmail">
+
+          <!-- Success/Error Message -->
+          <div v-if="message" class="mt-6 text-center">
+              <p class="font-medium" :class="{'text-green-500': success, 'text-red-500': !success}">
+                  {{ message }}
+              </p>
+          </div>
+
         <!-- Email Input -->
         <div class="mb-4">
-          <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-          <input 
-            v-model="email" 
-            type="email" 
-            id="email" 
-            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" 
-            required 
+          <label for="email" class="block text-sm font-medium">Email</label>
+          <input
+            v-model="email"
+            type="email"
+            id="email"
+            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            required
             placeholder="Votre email"
           />
         </div>
-        
+
         <!-- Name Input -->
         <div class="mb-4">
-          <label for="name" class="block text-sm font-medium text-gray-700">Nom</label>
-          <input 
-            v-model="name" 
-            type="text" 
-            id="name" 
-            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400" 
-            required 
+          <label for="name" class="block text-sm font-medium">Nom</label>
+          <input
+            v-model="name"
+            type="text"
+            id="name"
+            class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+            required
             placeholder="Nom de la personne"
           />
         </div>
-        
+
         <!-- Submit Button -->
-        <button 
-          type="submit" 
+        <button
+          type="submit"
           class="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
         >
           Envoyer l'email
         </button>
       </form>
 
-      <!-- Success/Error Message -->
-      <div v-if="message" class="mt-6 text-center">
-        <p class="text-lg font-medium" :class="{'text-green-500': success, 'text-red-500': !success}">
-          {{ message }}
-        </p>
-      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+.content {
+    --header-content-height: 40px;
+    --header-padding: 16px;
+    --header-border-width: 1px;
+    --header-margin: 20px;
+    --header-height: calc(var(--header-content-height) + var(--header-padding) * 2 + var(--header-border-width) + var(--header-margin));
+    --content-height: calc(100vh - var(--header-height));
+    height: max(var(--content-height), 600px);
+}
+</style>
