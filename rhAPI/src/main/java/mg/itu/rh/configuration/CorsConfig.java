008c97b176f8116cef166a79d599446ae7d8689c
henintsoapaul <henintsoapaul@gmail.com>
2026-02-17 21:14:42 +0300

    feat(back): cors config HTTPS

--- rhAPI/src/main/java/mg/itu/rh/configuration/CorsConfig.java
+++ rhAPI/src/main/java/mg/itu/rh/configuration/CorsConfig.java
@@ -8,13 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings( CorsRegistry registry ) {
-        registry.addMapping( "/**" )
-                .allowedOrigins( "http://localhost:3000" )
-                .allowedMethods( "GET", "POST", "PUT", "DELETE" )
-                .allowedHeaders( "*" )
-                .allowCredentials( true );
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
-
