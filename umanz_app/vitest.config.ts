717e70249f98c4b56f5c727139431498cbc7538c
henintsoapaul <henintsoapaul@gmail.com>
2026-04-04 21:40:28 +0300

    fix: prevent frontend unit test to run e2e tests

--- umanz_app/vitest.config.ts
+++ umanz_app/vitest.config.ts
@@ -5,5 +5,13 @@ export default defineVitestConfig({
     environment: 'nuxt',
     // any other vitest config you need
     globals: true,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+      'e2e/**'
+    ]
   }
 })
